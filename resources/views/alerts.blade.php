e0c3ec8f225f53a5ea4ebd2f05ec7d6b9e90c01c
Simon W <simon@apphaus.co.uk>
2023-09-25 17:15:44 +0100

    Working on kitchen sink for components

---
+++ resources/views/alerts.blade.php
@@ -0,0 +1,13 @@
+<h2>Alerts</h2>
+
+<div class="space-y-2">
+    <x-alert>This is a basic alert</x-alert>
+
+    <x-alert info>This is an info alert</x-alert>
+
+    <x-alert success>This is a successful alert</x-alert>
+
+    <x-alert warning>This is your last warning</x-alert>
+
+    <x-alert error>Something went wrong</x-alert>
+</div>
