f8812ea5a02fc29378e30f05b41b41965512b989
Simon W <simon@apphaus.co.uk>
2023-09-28 17:05:16 +0100

    Trying to deal with weird dev watch crash

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        watch: {
+            ignored: ['**/vendor/**']
+        }
+    },
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
 });
