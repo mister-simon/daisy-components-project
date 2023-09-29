c7d2e31d4c71e9871cba110d67f4bec5573c2569
Simon W <simon@apphaus.co.uk>
2023-09-29 16:41:43 +0100

    Adds modals section + helper component for styled top-right close button.

---
+++ resources/views/components/modal-close.blade.php
@@ -0,0 +1,3 @@
+<form method="dialog">
+    <x-button class="absolute right-2 top-2" circle ghost sm>✕</x-button>
+</form>
---
+++ resources/views/modals.blade.php
@@ -0,0 +1,84 @@
+<h2>Modals</h2>
+
+<h3>Basic Modal</h3>
+
+<div class="not-prose">
+    <x-modal>
+        @aware(['id'])
+        <x-button onclick="{{ $id }}.showModal()">Basic modal</x-button>
+        <x-slot:modal>Hello, I am the modal contents. Close me by pressing esc.</x-slot:modal>
+    </x-modal>
+</div>
+
+<h3>Cross Button Modal</h3>
+<div class="not-prose">
+    <x-modal>
+        @aware(['id'])
+        <x-button onclick="{{ $id }}.showModal()">Modal with ✕ button</x-button>
+        <x-slot:modal>
+            <x-modal-close />
+            Hello, I am the modal contents. Nice.
+        </x-slot:modal>
+    </x-modal>
+</div>
+
+<h3>Modal with Actions</h3>
+<div class="not-prose">
+    <x-modal>
+        @aware(['id'])
+        <x-button onclick="{{ $id }}.showModal()">Modal with a close action button</x-button>
+        <x-slot:modal>
+            Hello, I am the modal contents. Nice.
+        </x-slot:modal>
+        <x-slot:actions>
+            <form method="dialog">
+                <x-button>Close</x-button>
+            </form>
+        </x-slot:actions>
+    </x-modal>
+</div>
+
+<h3>Modal closing when backdrop is clicked</h3>
+<div class="not-prose">
+    <x-modal close-backdrop>
+        @aware(['id'])
+        <x-button onclick="{{ $id }}.showModal()">Modal with close backdrop</x-button>
+        <x-slot:modal>
+            <x-modal-close />
+            I'll close if you click the backdrop.
+        </x-slot:modal>
+    </x-modal>
+</div>
+
+<h3>Modal positions</h3>
+<div class="not-prose">
+    <x-modal top close-backdrop>
+        @aware(['id'])
+        <x-button onclick="{{ $id }}.showModal()">Modal top</x-button>
+        <x-slot:modal>
+            <x-modal-close />
+            Hello, I'm up here.
+        </x-slot:modal>
+    </x-modal>
+
+    <x-modal bottom close-backdrop>
+        @aware(['id'])
+        <x-button onclick="{{ $id }}.showModal()">Modal bottom</x-button>
+        <x-slot:modal>
+            <x-modal-close />
+            Hello, I'm down here.
+        </x-slot:modal>
+    </x-modal>
+
+    <x-modal close-backdrop bottom class="sm:modal-middle md:modal-top lg:modal-middle xl:modal-bottom 2xl:modal-middle">
+        @aware(['id'])
+        <x-button onclick="{{ $id }}.showModal()">DaisyUI responsive classes</x-button>
+        <x-slot:modal>
+            <x-modal-close />
+            <p class="mb-4">Reminder that DaisyUI responsive classes are a thing.</p>
+            <div class="mockup-code">
+                <pre><code>class="sm:modal-middle md:modal-top ... etc"</code></pre>
+            </div>
+        </x-slot:modal>
+    </x-modal>
+</div>
