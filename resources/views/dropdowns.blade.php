25f7c5b4c2373377f9601ae63ef2261aa6a018e2
Simon W <simon@apphaus.co.uk>
2023-09-29 16:40:43 +0100

    Add dropdowns section

--- resources/views/dropdowns.blade.php
+++ resources/views/dropdowns.blade.php
@@ -1,14 +1,79 @@
 <h2>Dropdowns</h2>
 
+<h3>Basic Dropdown</h3>
+
 <div class="not-prose">
-
     <x-dropdown>
-        <x-slot:summary>Well hia</x-slot:summary>
-        <x-slot:content>Oh my gosh!</x-slot:content>
+        <x-slot:summary class="btn-primary">Default dropdown</x-slot:summary>
+        <x-slot:content>
+            <ul class="menu rounded-box w-52 bg-base-100 p-2 shadow">
+                <li><a href="#">Oh my gosh!</a></li>
+                <li><a href="#">You can fit so many</a></li>
+                <li><a href="#">Dropdown items</a></li>
+                <li><a href="#">In this bad boy.</a></li>
+            </ul>
+        </x-slot:content>
     </x-dropdown>
+</div>
 
-    <x-dropdown open-on-hover>
-        <x-slot:summary>Hello</x-slot:summary>
-        <x-slot:content>Hi there</x-slot:content>
+<h3>Dropdown - Content alignment</h3>
+<div class="not-prose">
+    <x-dropdown top>
+        <x-slot:summary class="btn-primary">Top</x-slot:summary>
+        <x-slot:content>
+            <ul class="menu rounded-box w-52 bg-base-100 p-2 shadow">
+                <li><a href="#">Oh my gosh!</a></li>
+                <li><a href="#">Aligned to the Top</a></li>
+            </ul>
+        </x-slot:content>
+    </x-dropdown>
+    <x-dropdown bottom>
+        <x-slot:summary class="btn-primary">Bottom</x-slot:summary>
+        <x-slot:content>
+            <ul class="menu rounded-box w-52 bg-base-100 p-2 shadow">
+                <li><a href="#">Oh my gosh!</a></li>
+                <li><a href="#">Aligned to the Bottom</a></li>
+            </ul>
+        </x-slot:content>
+    </x-dropdown>
+    <x-dropdown left>
+        <x-slot:summary class="btn-primary">Left</x-slot:summary>
+        <x-slot:content>
+            <ul class="menu rounded-box w-52 bg-base-100 p-2 shadow">
+                <li><a href="#">Oh my gosh!</a></li>
+                <li><a href="#">Aligned to the Left</a></li>
+            </ul>
+        </x-slot:content>
+    </x-dropdown>
+    <x-dropdown right>
+        <x-slot:summary class="btn-primary">Right</x-slot:summary>
+        <x-slot:content>
+            <ul class="menu rounded-box w-52 bg-base-100 p-2 shadow">
+                <li><a href="#">Oh my gosh!</a></li>
+                <li><a href="#">Aligned to the Right</a></li>
+            </ul>
+        </x-slot:content>
+    </x-dropdown>
+    <x-dropdown end>
+        <x-slot:summary class="btn-primary">End</x-slot:summary>
+        <x-slot:content>
+            <ul class="menu rounded-box w-52 bg-base-100 p-2 shadow">
+                <li><a href="#">Oh my gosh!</a></li>
+                <li><a href="#">My contents are aligned to the end of the box</a></li>
+            </ul>
+        </x-slot:content>
+    </x-dropdown>
+</div>
+
+<h3>Dropdown - Forced Open</h3>
+<div class="not-prose">
+    <x-dropdown force-open>
+        <x-slot:summary class="btn-primary">Always open</x-slot:summary>
+        <x-slot:content>
+            <ul class="menu rounded-box w-52 bg-base-100 p-2 shadow">
+                <li><a href="#">Oh my gosh!</a></li>
+                <li><a href="#">I cannot be closed</a></li>
+            </ul>
+        </x-slot:content>
     </x-dropdown>
 </div>
