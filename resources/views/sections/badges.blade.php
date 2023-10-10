8582d4fd183abf7863daad68f77b49408b84df20
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-10 11:38:53 +0100

    Adds basics of badges section

---
+++ resources/views/sections/badges.blade.php
@@ -0,0 +1,40 @@
+<h2>Badges</h2>
+
+<h3>Badges with brand colors</h3>
+<div class="not-prose flex flex-wrap items-center gap-2">
+    <x-badge>badge</x-badge>
+    <x-badge neutral>Neutral</x-badge>
+    <x-badge primary>Primary</x-badge>
+    <x-badge secondary>Secondary</x-badge>
+    <x-badge accent>Accent</x-badge>
+    <x-badge ghost>Ghost</x-badge>
+    <x-badge info>Info</x-badge>
+    <x-badge success>Success</x-badge>
+    <x-badge warning>Warning</x-badge>
+    <x-badge error>Error</x-badge>
+</div>
+
+<h3>Outline badges</h3>
+
+<div class="not-prose flex flex-wrap items-center gap-2">
+    <x-badge outline>badge</x-badge>
+    <x-badge neutral outline>Neutral</x-badge>
+    <x-badge primary outline>Primary</x-badge>
+    <x-badge secondary outline>Secondary</x-badge>
+    <x-badge accent outline>Accent</x-badge>
+    <x-badge ghost outline>Ghost</x-badge>
+    <x-badge link outline>Link</x-badge>
+    <x-badge info outline>Info</x-badge>
+    <x-badge success outline>Success</x-badge>
+    <x-badge warning outline>Warning</x-badge>
+    <x-badge error outline>Error</x-badge>
+</div>
+
+<h3>Badge sizes</h3>
+
+<div class="not-prose flex flex-wrap items-center gap-2">
+    <x-badge lg>Large</x-badge>
+    <x-badge>Normal</x-badge>
+    <x-badge sm>Small</x-badge>
+    <x-badge xs>Tiny</x-badge>
+</div>
