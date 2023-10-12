df890014a89643e42de4426ef388e471a6099b36
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-12 13:56:34 +0100

    Adds collapse + docs for adding alpine to read the open state of components.

--- resources/views/sections/accordions.blade.php
+++ resources/views/sections/accordions.blade.php
@@ -1,55 +1,108 @@
 <h1>Accordions</h1>
 
-<div class="space-y-2">
-    <h2>Basic accordion</h2>
+<h2>Basic accordion</h2>
 
+<div class="space-y-2">
     @for ($i = 0; $i < 3; $i++)
         <x-accordion
-            class="border border-accent"
+            class="border border-neutral bg-base-200"
             :open="$i === 0">
             <x-slot:title>Click me to open up</x-slot:title>
-            <x-slot:content>This is my accordion contents.</x-slot:content>
+            This is my accordion contents.
         </x-accordion>
     @endfor
+</div>
 
-    <h2>Accordion (Plus)</h2>
-
+<h2>Accordion (Plus)</h2>
+<div class="space-y-2">
     @for ($i = 0; $i < 3; $i++)
         <x-accordion
-            class="border border-accent"
+            class="border border-neutral bg-base-200"
             :open="$i === 0"
             group="plus"
             plus>
             <x-slot:title>Click me to open up</x-slot:title>
-            <x-slot:content>This is my accordion contents.</x-slot:content>
+            This is my accordion contents.
         </x-accordion>
     @endfor
-    <h2>Accordion (Arrow)</h2>
+</div>
 
+<h2>Accordion (Arrow)</h2>
+<div class="space-y-2">
     @for ($i = 0; $i < 3; $i++)
         <x-accordion
-            class="border border-accent"
+            class="border border-neutral bg-base-200"
             :open="$i === 0"
             group="arrow"
             arrow>
             <x-slot:title>Click me to open up</x-slot:title>
-            <x-slot:content>This is my accordion contents.</x-slot:content>
+            This is my accordion contents.
         </x-accordion>
     @endfor
+</div>
 
-    <h2>Joined accordion</h2>
+<h2>Joined accordion</h2>
+<div class="join join-vertical w-full">
+    @for ($i = 0; $i < 3; $i++)
+        <x-accordion
+            class="join-item border border-neutral bg-base-200"
+            :open="$i === 0"
+            group="joined"
+            arrow>
+            <x-slot:title>Click me to open up</x-slot:title>
+            This is my accordion contents.
+        </x-accordion>
+    @endfor
+</div>
 
-    <div class="join join-vertical w-full">
-        @for ($i = 0; $i < 3; $i++)
-            <x-accordion
-                class="join-item border border-accent"
-                :open="$i === 0"
-                group="joined"
-                arrow>
-                <x-slot:title>Click me to open up</x-slot:title>
-                <x-slot:content>This is my accordion contents.</x-slot:content>
-            </x-accordion>
-        @endfor
-    </div>
+<h2>Styled accordion</h2>
+<div class="space-y-2">
+    @for ($i = 0; $i < 3; $i++)
+        <x-accordion
+            class="join-item border border-neutral bg-base-200"
+            :open="$i === 0"
+            group="joined"
+            arrow>
+            <x-slot:title class="bg-neutral text-neutral-content peer-checked:bg-info peer-checked:text-info-content">Apply styles when selected.</x-slot:title>
+            <x-slot:content class="bg-neutral text-neutral-content peer-checked:bg-info peer-checked:text-info-content">This is done using the <code>peer-checked:</code> tailwind selector. You may add classes to title / content slots.</x-slot:content>
+        </x-accordion>
+    @endfor
+</div>
 
+<h2>Accordion with Alpine</h2>
+<div
+    class="space-y-2"
+    x-data="{
+        open: '0',
+        isOpen(id) { return this.open === id }
+    }">
+    @for ($i = 0; $i < 3; $i++)
+        <x-accordion
+            x-data="{ id: '{{ $i }}' }"
+            class="border border-neutral bg-base-200"
+            :open="$i === 0"
+            group="alpine">
+            <x-slot:radio
+                x-model="open"
+                x-bind:value="id">
+            </x-slot:radio>
+
+            <x-slot:title>
+                This is <span x-text="isOpen(id) ? 'open' : 'closed'"></span>
+            </x-slot:title>
+
+            <div class="prose-sm">
+                <p>How you implement this is really up to you... But for this example:</p>
+                <ul>
+                    <li>A wrapper tracks which item is checked (only one item may be selected at a time).</li>
+                    <li>Note that alpine casts input values to strings, so a selected id of 1 is actually "1".</li>
+                    <li>We have given the accordion item an id via <code>x-data</code>, for easy access within alpine.</li>
+                    <li>Next, the <code>&lt;x-slot:radio&gt;</code> slot is used to set the alpine model connection to the wrappers <code>open</code> state.</li>
+                    <li>Finally, the accordion item can check if it is open using the helper method <code>isOpen</code> provided by the wrapper.</li>
+                </ul>
+                <p>I've left this implementation intentionally sparse, not making assumptions about your use case / forcing patterns that might not work in the long run.</p>
+            </div>
+
+        </x-accordion>
+    @endfor
 </div>
---
+++ resources/views/sections/collapse.blade.php
@@ -0,0 +1,81 @@
+<h1>Collapses</h1>
+
+<h2>Basic collapse</h2>
+
+<x-collapse
+    class="border border-neutral bg-base-200">
+    <x-slot:title>Click me to open up</x-slot:title>
+    This is my collapse contents.
+</x-collapse>
+
+<h2>Collapse (Plus)</h2>
+
+<x-collapse
+    class="border border-neutral bg-base-200"
+    plus>
+    <x-slot:title>Click me to open up</x-slot:title>
+    This is my collapse contents.
+</x-collapse>
+
+<h2>Collapse (Arrow)</h2>
+
+<x-collapse
+    class="border border-neutral bg-base-200"
+
+    arrow>
+    <x-slot:title>Click me to open up</x-slot:title>
+    This is my collapse contents.
+</x-collapse>
+
+<h2>Joined collapse</h2>
+
+<div class="join join-vertical w-full">
+    @for ($i = 0; $i < 3; $i++)
+        <x-collapse
+            class="join-item border border-neutral bg-base-200"
+            arrow>
+            <x-slot:title>Click me to open up</x-slot:title>
+            This is my collapse contents.
+        </x-collapse>
+    @endfor
+</div>
+
+<h2>Styled collapse</h2>
+
+<x-collapse
+    class="join-item border border-neutral bg-base-200"
+    arrow>
+    <x-slot:title class="bg-neutral text-neutral-content peer-checked:bg-info peer-checked:text-info-content">Apply styles when selected.</x-slot:title>
+    <x-slot:content class="bg-neutral text-neutral-content peer-checked:bg-info peer-checked:text-info-content">This is done using the <code>peer-checked:</code> selector, passing classes to title / content slots.</x-slot:content>
+</x-collapse>
+
+<h2>Collapse with Alpine</h2>
+
+<div class="space-y-2">
+    @for ($i = 0; $i < 3; $i++)
+        <x-collapse
+            x-data="{ open: {{ (int) ($i === 0) }} }"
+            class="border border-neutral bg-base-200"
+            :open="$i === 0">
+            <x-slot:checkbox
+                x-model="open">
+            </x-slot:checkbox>
+
+            <x-slot:title>
+                This is <span x-text="open ? 'open' : 'closed'"></span>
+            </x-slot:title>
+
+            <div class="prose-sm">
+                <p>How you implement this is really up to you... But for this example:</p>
+                <ul>
+                    <li>Collapse tracks it's own open state.</li>
+                    <li>Note, to ensure the open state matches that of the <code>:open</code> attribute, that is set separately in the initial <code>x-data</code>.</li>
+                    <li>Next, the <code>&lt;x-slot:checkbox&gt;</code> slot is used to connect the checkbox to the alpine <code>open</code> state.</li>
+                    <li>We can then freely use the <code>open</code> state within our collapse component.</li>
+                </ul>
+                <p>I've left this implementation intentionally sparse, not making assumptions about your use case / forcing patterns that might not work in the long run.</p>
+            </div>
+
+        </x-collapse>
+    @endfor
+</div>
