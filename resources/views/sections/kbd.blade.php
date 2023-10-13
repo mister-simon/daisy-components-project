e7ca78a07d34166f012e1496a21dbcd383a31b25
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-13 16:16:57 +0100

    Formatting / not-prose tweaks

--- resources/views/sections/kbd.blade.php
+++ resources/views/sections/kbd.blade.php
@@ -2,28 +2,45 @@
 
 <h2>Kbd</h2>
 
-<x-kbd>A</x-kbd>
+<div class="not-prose">
+    <x-kbd>A</x-kbd>
+</div>
 
 <h2>Kbd sizes</h2>
 
-<x-kbd lg>Large</x-kbd>
-<x-kbd md>Medium</x-kbd>
-<x-kbd>Default</x-kbd>
-<x-kbd sm>Small</x-kbd>
-<x-kbd xs>X-Small</x-kbd>
+<div class="not-prose">
+    <x-kbd lg>Large</x-kbd>
+    <x-kbd md>Medium</x-kbd>
+    <x-kbd>Default</x-kbd>
+    <x-kbd sm>Small</x-kbd>
+    <x-kbd xs>X-Small</x-kbd>
+</div>
 
 <h2>Inline kbd</h2>
 
-<p>Press <x-kbd sm>F</x-kbd> to pay respects</p>
+<div class="not-prose">
+    <p>Press <x-kbd sm>F</x-kbd> to pay respects</p>
+</div>
 
 <h2>Key Combos</h2>
 
-<p>
-    <kbd><x-kbd>ctrl</x-kbd> + <x-kbd>alt</x-kbd> + <x-kbd>del</x-kbd></kbd>
-</p>
+<div class="not-prose">
+    <p>
+        You can close your browser by pressing
+        <kbd><x-kbd sm>alt</x-kbd> + <x-kbd sm>F4</x-kbd></kbd>.
+    </p>
+    <p>Or on a mac press
+        <kbd><x-kbd sm>⌘ cmd</x-kbd> + <x-kbd sm>Q</x-kbd></kbd>.
+    </p>
+</div>
 
 <h2>Function keys</h2>
-<x-kbd>⌘</x-kbd>
-<x-kbd>⌥</x-kbd>
-<x-kbd>⇧</x-kbd>
-<x-kbd>⌃</x-kbd>
+
+<div class="not-prose">
+    <p>
+        <x-kbd>⌘</x-kbd>
+        <x-kbd>⌥</x-kbd>
+        <x-kbd>⇧</x-kbd>
+        <x-kbd>⌃</x-kbd>
+    </p>
+</div>
