13a19e4bdcd4dfad4db84b96eae5bcb933180175
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-13 11:21:09 +0100

    Adds kbd section

---
+++ resources/views/sections/kbd.blade.php
@@ -0,0 +1,29 @@
+<h1>Kbd</h1>
+
+<h2>Kbd</h2>
+
+<x-kbd>A</x-kbd>
+
+<h2>Kbd sizes</h2>
+
+<x-kbd lg>Large</x-kbd>
+<x-kbd md>Medium</x-kbd>
+<x-kbd>Default</x-kbd>
+<x-kbd sm>Small</x-kbd>
+<x-kbd xs>X-Small</x-kbd>
+
+<h2>Inline kbd</h2>
+
+<p>Press <x-kbd sm>F</x-kbd> to pay respects</p>
+
+<h2>Key Combos</h2>
+
+<p>
+    <kbd><x-kbd>ctrl</x-kbd> + <x-kbd>alt</x-kbd> + <x-kbd>del</x-kbd></kbd>
+</p>
+
+<h2>Function keys</h2>
+<x-kbd>⌘</x-kbd>
+<x-kbd>⌥</x-kbd>
+<x-kbd>⇧</x-kbd>
+<x-kbd>⌃</x-kbd>
