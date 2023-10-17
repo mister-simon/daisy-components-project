8f3d7a5461022c34df50465b270de36d92f6e8de
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-17 16:32:55 +0100

    Adds text inputs examples

---
+++ resources/views/sections/text-inputs.blade.php
@@ -0,0 +1,63 @@
+<h1>Text Inputs</h1>
+
+<h2>Basic input</h2>
+
+<div class="not-prose rounded-box flex flex-col items-start gap-4 bg-base-200 p-4">
+    <x-input placeholder="I am default" />
+    <x-input placeholder="I have a border" bordered />
+    <x-input placeholder="I have no background" ghost />
+    <x-input placeholder="This input is disabled" bordered disabled />
+</div>
+
+<h2>Label + form control</h2>
+
+<div class="not-prose rounded-box bg-base-200 p-4">
+    <x-form-control>
+        <x-label
+            text="This is the label for the input"
+            text-alt="And maybe this is helper text?"
+            for="e1" />
+        <x-input
+            bordered
+            id="e1"
+            value="Don't forget to link up your labels. Otherwise, what's the point." />
+    </x-form-control>
+</div>
+
+<h2>Restyled label</h2>
+
+<div class="not-prose rounded-box bg-base-200 p-4">
+    <x-form-control>
+        <x-label for="e2">
+            <x-slot:text class="badge badge-info badge-lg">You can restyle label text</x-slot:text>
+            <x-slot:textAlt class="badge badge-success badge-lg">What a surprise</x-slot:textAlt>
+        </x-label>
+        <x-input
+            bordered
+            id="e2"
+            value="You might prefer to keep things consistent though."
+            sm />
+    </x-form-control>
+</div>
+
+<h2>Sizes</h2>
+
+<div class="not-prose rounded-box flex flex-col items-start gap-4 bg-base-200 p-4">
+    <x-input lg value="Large" />
+    <x-input md value="Medium / Default" />
+    <x-input sm value="Small" />
+    <x-input xs value="Extra-Small" />
+</div>
+
+<h2>Colours</h2>
+
+<div class="not-prose rounded-box flex flex-wrap gap-4 bg-base-200 p-4">
+    <x-input lg value="default" />
+    <x-input lg primary value="primary" />
+    <x-input lg secondary value="secondary" />
+    <x-input lg accent value="accent" />
+    <x-input lg info value="info" />
+    <x-input lg success value="success" />
+    <x-input lg warning value="warning" />
+    <x-input lg error value="error" />
+</div>
