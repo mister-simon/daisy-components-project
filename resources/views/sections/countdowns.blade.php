3b490f8f20d49166f0300dcc3f72ef6ddee41370
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-19 17:41:01 +0100

    Fixes some countdowns + adds some dev notes.

--- resources/views/sections/countdowns.blade.php
+++ resources/views/sections/countdowns.blade.php
@@ -6,12 +6,14 @@
 
 <h2>Basic live countdown</h2>
 
+{{-- Countdown will always initialise as an alpine component, exposing a "count" variable. --}}
 <x-countdown
     :count="10"
     x-init="setInterval(() => count = count === 0 ? 99 : count - 1, 1000)" />
 
 <h2>Static using child components</h2>
 
+{{-- You may use countdown-item sub-components for cases where that may be useful. --}}
 <p>
     Today I am
     <x-countdown>
@@ -21,10 +23,20 @@
 </p>
 
 <h2>Startup effect</h2>
-
+{{-- Without the timeout it seems that the countdown is rendered before the cloak is removed. --}}
+{{-- Thereby preventing the intended startup effect. --}}
+{{-- NOTE: [x-cloak] will need to be added to your styles to take effect. --}}
 <p>
     Today I am
-    <x-countdown :count="0" x-init="count = 30" x-cloak /> - <x-countdown :count="99" x-init="count = 35" x-cloak />
+    <x-countdown
+        :count="0"
+        x-init="setTimeout(() => count = 30, 0)"
+        x-cloak />
+    -
+    <x-countdown
+        :count="99"
+        x-init="setTimeout(() => count = 35, 0)"
+        x-cloak />
     <span x-data x-show="false">30 - 35</span>
     years old.
 </p>
@@ -36,6 +48,7 @@
 
 <h2>Live countdown with child component</h2>
 
+{{-- You can override the default alpine interactions to have greater control --}}
 <x-countdown
     x-data="{ countdown: 10 }"
     x-init="setInterval(() => countdown = countdown === 0 ? 99 : countdown - 1, 1000)">
@@ -46,6 +59,7 @@
 
 <h2>Countdown to display time</h2>
 
+{{-- This example combines many of the features above --}}
 <x-countdown
     class="font-mono text-2xl"
     x-data="{
@@ -56,9 +70,10 @@
                 d.getHours(),
                 d.getMinutes(),
                 d.getSeconds(),
-            ]
+            ];
         }
     }"
-    x-init="updateTime() && setInterval(() => updateTime(), 1000)">
+    x-init="updateTime();
+    setInterval(() => updateTime(), 1000);">
     <x-countdown-item :count="12" x-model="time[0]" />:<x-countdown-item :count="30" x-model="time[1]" />:<x-countdown-item :count="30" x-model="time[2]" />
 </x-countdown>
