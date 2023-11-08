d6e99c9dce6e5e98ebc258c24bc5098f9ea438ce
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-11-08 11:48:15 +0000

    Formatting

--- resources/views/pages/[section].blade.php
+++ resources/views/pages/[section].blade.php
@@ -1,3 +1,5 @@
-<x-app.layout :title="'Daisy Components - ' . $section" :subtitle="str($section)->title()">
+<x-app.layout
+    :title="'Daisy Components - ' . $section"
+    :subtitle="str($section)->title()">
     @includeFirst(["sections.{$section}", 'partials.component-not-found'])
 </x-app.layout>
