d44302630e53bf6738881f58b94b221df6c48305
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-11-08 11:48:24 +0000

    Adds breadcrumbs example

---
+++ resources/views/sections/breadcrumbs.blade.php
@@ -0,0 +1,17 @@
+<p>This simply provides a wrapper for a single class: <code>breadcrumbs</code>.<br>
+    It's here for completeness, but I would recommend simply using the breadcrumbs class directly.<br>
+    Refer to the
+    <a href="https://daisyui.com/components/breadcrumbs/" target="_blank" rel="noopener noreferrer">DaisyUI Documentation for the Breadcrumbs component</a>.
+</p>
+
+<h2>Basic breadcrumbs list</h2>
+
+<div class="not-prose">
+    <x-breadcrumbs>
+        <ul>
+            <li>DaisyUI Components</li>
+            <li>Home</li>
+            <li>Breadcrumbs</li>
+        </ul>
+    </x-breadcrumbs>
+</div>
